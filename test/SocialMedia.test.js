ffd86a960b7c70161c76e9c05aa124e59e0d76d3
Karthik-Gupta <karthikts.gupta@gmail.com>
2021-10-10 10:43:36 +0530

    more test cases WIP

--- test/SocialMedia.test.js
+++ test/SocialMedia.test.js
@@ -27,4 +27,34 @@ describe("Working with Post", function() {
         const postArray = await socialMedia.getPostsByUser(creator.address);
         expect(postArray.map(e => e.description)).to.include("test description");
     });
+
+    it ("Get all posts", async function() {
+        expect(await socialMedia.getAllPosts()).to.have.lengthOf(1);
+    });
+});
+
+describe("Working with voting", function() {
+    it("Caste a up vote", async function() {
+        await socialMedia.vote(1, true);
+        const vote = await socialMedia.getVoteByPostId(1);
+        expect(vote).to.have.property('upVote');
+        expect(vote.upVote).to.equal(1);
+    });
+
+    it("Caste a down vote", async function() {
+        await socialMedia.vote(1, false);
+        const vote = await socialMedia.getVoteByPostId(1);
+        expect(vote).to.have.property('downVote');
+        expect(vote.downVote).to.equal(1);
+    });
+
+    it ("Check the both up vote and own dvote increment", async function() {
+        await socialMedia.vote(1, true);
+        await socialMedia.vote(1, false);
+        const vote = await socialMedia.getVoteByPostId(1);
+        expect(vote).to.have.property('upVote');
+        expect(vote.upVote).to.equal(2);
+        expect(vote).to.have.property('downVote');
+        expect(vote.downVote).to.equal(2);
+    });
 });
